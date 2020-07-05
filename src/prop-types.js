30540d8fc5e7b62e5970fac369cf9dc5c8b67c81
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-05 17:55:04 +0300

    Поправит PropTypes для MoviePropType.

--- src/prop-types.js
+++ src/prop-types.js
@@ -2,19 +2,19 @@ import PropTypes from "prop-types";
 
 
 export const MoviePropType = PropTypes.shape({
-  title: PropTypes.string,
-  smallPictureUrl: PropTypes.string,
-  backgroundUrl: PropTypes.string,
-  posterUrl: PropTypes.string,
-  genres: PropTypes.arrayOf(PropTypes.string),
-  releaseDate: PropTypes.instanceOf(Date),
-  description: PropTypes.arrayOf(PropTypes.string),
-  directors: PropTypes.arrayOf(PropTypes.string),
-  starring: PropTypes.arrayOf(PropTypes.string),
-  runTime: PropTypes.number,
-  reviews: PropTypes.array,
+  title: PropTypes.string.isRequired,
+  smallPictureUrl: PropTypes.string.isRequired,
+  backgroundUrl: PropTypes.string.isRequired,
+  posterUrl: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  releaseDate: PropTypes.instanceOf(Date).isRequired,
+  description: PropTypes.arrayOf(PropTypes.string).isRequired,
+  directors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  starring: PropTypes.arrayOf(PropTypes.string).isRequired,
+  runTime: PropTypes.number.isRequired,
+  reviews: PropTypes.array.isRequired,
   rating: PropTypes.shape({
-    score: PropTypes.number,
-    totalVotes: PropTypes.number,
-  }),
+    score: PropTypes.number.isRequired,
+    totalVotes: PropTypes.number.isRequired,
+  }).isRequired,
 });

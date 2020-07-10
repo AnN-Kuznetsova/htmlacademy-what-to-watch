68c085dae66ff2802a9ccb70f531ddc8737b9c29
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-10 18:39:14 +0300

    Перенесет 'MainPageHeader' и 'MovieDetailsPageHeader' в 'MovieCardPromo' и 'MovieCardFull' и удалит их компоненты.

--- src/components/movie-card-full/movie-card-full.jsx
+++ src/components/movie-card-full/movie-card-full.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import {MovieDetailsPageHeader} from "../movie-details-page-header/movie-details-page-header";
 import {MoviePropType} from "../../prop-types";
 import {
   getParticipantsLine,
@@ -20,10 +19,59 @@ export const MovieCardFull = (props) => {
 
   return (
     <section className="movie-card movie-card--full">
-      <MovieDetailsPageHeader
-        movie={movie}
-        onMovieClick={null}
-      />
+      <div className="movie-card__hero">
+        <div className="movie-card__bg">
+          <img src={movie.backgroundUrl} alt={movie.title} />
+        </div>
+
+        <h1 className="visually-hidden">WTW</h1>
+
+        <header className="page-header movie-card__head">
+          <div className="logo">
+            <a href="/" className="logo__link">
+              <span className="logo__letter logo__letter--1">W</span>
+              <span className="logo__letter logo__letter--2">T</span>
+              <span className="logo__letter logo__letter--3">W</span>
+            </a>
+          </div>
+
+          <div className="user-block">
+            <div className="user-block__avatar">
+              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+            </div>
+          </div>
+        </header>
+
+        <div className="movie-card__wrap">
+          <div className="movie-card__desc">
+            <h2
+              className="movie-card__title"
+            >
+              {movie.title}
+            </h2>
+            <p className="movie-card__meta">
+              <span className="movie-card__genre">{movie.genres[0]}</span>
+              <span className="movie-card__year">{movie.releaseDate.getFullYear()}</span>
+            </p>
+
+            <div className="movie-card__buttons">
+              <button className="btn btn--play movie-card__button" type="button">
+                <svg viewBox="0 0 19 19" width="19" height="19">
+                  <use xlinkHref="#play-s" />
+                </svg>
+                <span>Play</span>
+              </button>
+              <button className="btn btn--list movie-card__button" type="button">
+                <svg viewBox="0 0 19 20" width="19" height="20">
+                  <use xlinkHref="#add" />
+                </svg>
+                <span>My list</span>
+              </button>
+              <a href="add-review.html" className="btn movie-card__button">Add review</a>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <div className="movie-card__wrap movie-card__translate-top">
         <div className="movie-card__info">

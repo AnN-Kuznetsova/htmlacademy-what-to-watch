abd9fabd8ae45e1ac03740a62fb9023f68ba54ae
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-11 22:21:36 +0300

    Отрефакторит теги без тела.

--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -72,8 +72,7 @@ export class VideoPlayer extends PureComponent {
           poster={posterUrl}
           autoPlay={isPlaying}
           style={{height: videoHeight}}
-        >
-        </video>
+        />
 
         {isFullScreen && <button type="button" className="player__exit">Exit</button>}
 
@@ -81,7 +80,7 @@ export class VideoPlayer extends PureComponent {
           <div className="player__controls">
             <div className="player__controls-row">
               <div className="player__time">
-                <progress className="player__progress" value="30" max="100"></progress>
+                <progress className="player__progress" value="30" max="100" />
                 <div className="player__toggler" style={{left: `30%`}}>Toggler</div>
               </div>
               <div className="player__time-value">1:30:29</div>
@@ -90,7 +89,7 @@ export class VideoPlayer extends PureComponent {
             <div className="player__controls-row">
               <button type="button" className="player__play">
                 <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use xlinkHref="#play-s"></use>
+                  <use xlinkHref="#play-s" />
                 </svg>
                 <span>Play</span>
               </button>
@@ -98,7 +97,7 @@ export class VideoPlayer extends PureComponent {
 
               <button type="button" className="player__full-screen">
                 <svg viewBox="0 0 27 27" width="27" height="27">
-                  <use xlinkHref="#full-screen"></use>
+                  <use xlinkHref="#full-screen" />
                 </svg>
                 <span>Full screen</span>
               </button>

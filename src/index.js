b2b0a7824bcec942a750d6382201ea30ddd27312
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-13 18:27:31 +0300

    Добавит подключение Redux DevTools.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import {App} from "./components/app/app";
 import {reducer} from "./reducers/reducer";
 
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 
 ReactDom.render(

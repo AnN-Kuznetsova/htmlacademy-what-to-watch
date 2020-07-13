99dc4528011bebe779921ca5d18744e9b0a77ce7
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-13 18:25:28 +0300

    Доббавит 'store'.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDom from "react-dom";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+
 import {App} from "./components/app/app";
+import {reducer} from "./reducers/reducer";
+
+
+const store = createStore(reducer);
+
 
 ReactDom.render(
-    <App />,
+    <Provider store={store}>
+      <App />
+    </Provider>,
     document.querySelector(`#root`)
 );

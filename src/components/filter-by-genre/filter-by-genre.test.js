879fea7341cca9b10bf8393b42ee89359202edda
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-16 13:24:07 +0300

    Создаст 'FilterByGenre'.

---
+++ src/components/filter-by-genre/filter-by-genre.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import {FilterByGenreComponent} from "./filter-by-genre";
+
+import {mockMovies} from "../../__test-data__/test-mocks";
+
+
+const props = {
+  movies: mockMovies,
+  activeFilter: `All genres`,
+  onClick: () => {},
+};
+
+
+describe(`Render FilterByGenre`, () => {
+  it(`FilterByGenre should match with snapshot`, () => {
+    const filterByGenreSnapshot = renderer.create(
+        <FilterByGenreComponent {...props} />
+    ).toJSON();
+
+    expect(filterByGenreSnapshot).toMatchSnapshot();
+  });
+})

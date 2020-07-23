4753e781c2949b62e65b74f11d380523d9acd663
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-24 01:41:18 +0300

    Зафиксирует время по гринвичу в моке.

--- src/__test-data__/test-mocks.js
+++ src/__test-data__/test-mocks.js
@@ -7,7 +7,7 @@ const mockPromoMovie = {
   posterUrl: `img/the-grand-budapest-hotel-poster.jpg`,
   previewUrl: mockVideoUrl,
   genres: [`Drama`, `Comedy`],
-  releaseDate: new Date(2014, 0),
+  releaseDate: new Date(`Wed Jan 01 2014 00:00:00 GMT+0`),
   description: [
     `In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy,
     becomes Gustave's friend and protege.`,

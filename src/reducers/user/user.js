0db312f6adf8a897195708b54bed55745374b1e7
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-24 23:30:44 +0300

    Напишет редьюсер для user и тесты к нему.

---
+++ src/reducers/user/user.js
@@ -0,0 +1,65 @@
+import {extend} from "../../utils/utils";
+
+
+const AuthorizationStatus = {
+  AUTH: `AUTH`,
+  NO_AUTH: `NO_AUTH`,
+};
+
+const initialState = {
+  authorizationStatus: AuthorizationStatus.NO_AUTH,
+};
+
+const ActionType = {
+  REQUIRED_AUTHORIZATION: `REQUIRED_AUTHORIZATION`,
+};
+
+const ActionCreator = {
+  requireAuthorization: (status) => ({
+    type: ActionType.REQUIRED_AUTHORIZATION,
+    payload: status,
+  }),
+};
+
+const Operation = {
+  checkAuth: () => (dispatch, getState, api) => {
+    return api.get(`/login`)
+      .then(() => {
+        dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
+      })
+      .catch((err) => {
+        throw err;
+      });
+  },
+
+  login: (authData) => (dispatch, getState, api) => {
+    return api.post(`/login`, {
+      email: authData.login,
+      password: authData.password,
+    })
+    .then(() => {
+      dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH));
+    });
+  },
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.REQUIRED_AUTHORIZATION:
+      return extend(state, {
+        authorizationStatus: action.payload,
+      });
+
+    default:
+      return state;
+  }
+};
+
+
+export {
+  ActionCreator,
+  ActionType,
+  AuthorizationStatus,
+  Operation,
+  reducer,
+};
---
+++ src/reducers/user/user.test.js
@@ -0,0 +1,136 @@
+import MockAdapter from "axios-mock-adapter";
+
+import {createAPI} from "../../api.js";
+import {reducer, ActionType, ActionCreator, AuthorizationStatus, Operation} from "./user.js";
+
+
+describe(`User reducer should work correctly`, () => {
+  it(`User reducer without additional parameters should return initial state`, () => {
+    expect(reducer(void 0, {})).toEqual({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    });
+  });
+
+
+  it(`User reducer should change authorizationStatus by a given value`, () => {
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    });
+
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    });
+
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.AUTH,
+    });
+
+    expect(reducer({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    }, {
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    })).toEqual({
+      authorizationStatus: AuthorizationStatus.NO_AUTH,
+    });
+  });
+});
+
+
+describe(`User action creators should work correctly`, () => {
+  it(`User action creator for require authorization returns correct action`, () => {
+    expect(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH)).toEqual({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    });
+
+    expect(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)).toEqual({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    });
+  });
+});
+
+
+describe(`User operation work correctly`, () => {
+  it(`Should make a correct API call to /login on positive authentication check`, () => {
+    const api = createAPI(() => {});
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const authVerifier = Operation.checkAuth();
+
+    apiMock
+      .onGet(`/login`)
+      .reply(200);
+
+    return authVerifier(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+      });
+  });
+
+
+  it(`Should make a correct API call to /login on negative authentication check`, () => {
+    const api = createAPI(() => {});
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const authVerifier = Operation.checkAuth();
+
+    apiMock
+      .onGet(`/login`)
+      .reply(400, {data: false});
+
+    return authVerifier(dispatch, () => {}, api)
+      .then()
+      .catch(() => {
+        expect(dispatch).toHaveBeenCalledTimes(0);
+      });
+  });
+
+
+  it(`Should make a correct API call to /login with post request`, () => {
+    const api = createAPI(() => {});
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const authData = {
+      login: `login`,
+      password: `password`,
+    };
+    const loginSender = Operation.login(authData);
+
+    apiMock
+      .onPost(`/login`, {
+        email: authData.login,
+        password: authData.password,
+      })
+      .reply(200);
+
+    return loginSender(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+      });
+  });
+});

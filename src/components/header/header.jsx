1a68bea47b506e5860f6560b82d6c2fd9438db8b
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-07-27 19:49:21 +0300

    Рефакторит 'Header'.

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -10,6 +10,28 @@ import {getAuthorizationStatus} from "../../reducers/user/selectors";
 import {getActivePage} from "../../reducers/application/selectors";
 
 
+const HeaderMode = {
+  ERROR_PAGE: `ERROR_PAGE`,
+  SIGN_IN_PAGE: `SIGN_IN_PAGE`,
+  MOVIE_CARD_AUTH: `MOVIE_CARD_AUTH`,
+  MOVIE_CARD_NO_AUTH: `MOVIE_CARD_NO_AUTH`,
+};
+
+const getHeaderMode = (activePage, authorizationStatus) => {
+  switch (true) {
+    case activePage === PageType.ERROR:
+      return HeaderMode.ERROR_PAGE;
+    case activePage === PageType.SIGN_IN:
+      return HeaderMode.SIGN_IN_PAGE;
+    case authorizationStatus === AuthorizationStatus.AUTH:
+      return HeaderMode.MOVIE_CARD_AUTH;
+    case authorizationStatus === AuthorizationStatus.NO_AUTH:
+    default:
+      return HeaderMode.MOVIE_CARD_NO_AUTH;
+  }
+};
+
+
 const HeaderComponent = (props) => {
   const {
     authorizationStatus,
@@ -17,9 +39,7 @@ const HeaderComponent = (props) => {
     onOpenSignInPage,
   } = props;
 
-  const isAuth = authorizationStatus === AuthorizationStatus.AUTH;
-  const isSignInPage = activePage === PageType.SIGN_IN;
-  const isErrorPage = activePage === PageType.ERROR;
+  const headerMode = getHeaderMode(activePage, authorizationStatus);
 
   const handleSignInClick = (event) => {
     event.preventDefault();
@@ -30,24 +50,24 @@ const HeaderComponent = (props) => {
     <React.Fragment>
       <h1 className="visually-hidden">WTW</h1>
 
-      <header className={`page-header ${isAuth && !isSignInPage && `movie-card__head`} ${isSignInPage && `user-page__head`}`}>
+      <header className={`page-header ${headerMode === HeaderMode.MOVIE_CARD_AUTH && `movie-card__head`} ${headerMode === HeaderMode.SIGN_IN_PAGE && `user-page__head`}`}>
         <Logo mode={LogoMode.NORMAL} />
 
-        {isAuth && !isSignInPage && !isErrorPage &&
+        {headerMode === HeaderMode.MOVIE_CARD_AUTH &&
           <div className="user-block">
             <div className="user-block__avatar">
               <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
             </div>
           </div>}
 
-        {!isAuth && !isSignInPage && !isErrorPage &&
+        {headerMode === HeaderMode.MOVIE_CARD_NO_AUTH &&
           <div className="user-block">
             <a href="sign-in.html" className="user-block__link"
               onClick={handleSignInClick}
             >Sign in</a>
           </div>}
 
-        {isSignInPage &&
+        {headerMode === HeaderMode.SIGN_IN_PAGE &&
           <h1 className="page-title user-page__title">Sign in</h1>}
       </header>
     </React.Fragment>

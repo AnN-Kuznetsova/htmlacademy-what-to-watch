787fd600563dd252fd0e0c6ffc01aef3ecf5e6aa
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-08-05 13:50:41 +0300

    Рефакторит 'MainPage'.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -4,41 +4,31 @@ import {Switch, Route, Router, Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 
 import {ActionCreator as ApplicationActionCreator} from "../../reducers/application/application";
-import {ActionCreator as DataActionCtrator, Operation} from "../../reducers/data/data";
+import {ActionCreator as DataActionCtrator} from "../../reducers/data/data";
 import {AddReviewPageWithNewReview} from "../add-review-page/add-review-page";
 import {ErrorPage} from "../error-page/error-page";
 import {MainPage} from "../main-page/main-page";
 import {MovieDetailsPage} from "../movie-details-page/movie-details-page";
 import {MoviePropType} from "../../prop-types";
-import {PageType, NUMBER_OF_SIMILAR_FILMS, AppRoute} from "../../const";
+import {PageType, AppRoute} from "../../const";
 import {PlayerPage} from "../player-page/player-page";
 import {PrivateRoute} from "../private-route/private-route";
 import {RedirectToMainRoute} from "../redirect-to-main-route/redirect-to-main-route";
 import {SignIn} from "../sign-in/sign-in";
 import {getActivePage} from "../../reducers/application/selectors";
-import {getDataError, getMovies, getPromoMovie} from "../../reducers/data/selectors";
+import {getDataError, getMovies} from "../../reducers/data/selectors";
 import {history} from "../../history";
 
 
 class AppComponent extends PureComponent {
-  componentDidMount() {
-    const path = window.location.pathname;
+  /* componentDidMount() {
 
-    switch (true) {
-      case path === AppRoute.MAIN:
-        this.props.changeActiveMovie(this.props.promoMovie);
-        break;
-
-      default:
-        break;
-    }
-  }
+  } */
 
   renderPage() {
     const {
       dataError,
       activePage,
-      promoMovie,
     } = this.props;
 
     window.scrollTo(0, 0);
@@ -46,7 +36,7 @@ class AppComponent extends PureComponent {
     switch (activePage) {
       case PageType.MAIN:
         return (
-          <MainPage promoMovie={promoMovie} />
+          <MainPage />
         );
 
       case PageType.ERROR:
@@ -119,44 +109,24 @@ class AppComponent extends PureComponent {
 AppComponent.propTypes = {
   dataError: PropTypes.object,
   activePage: PropTypes.string.isRequired,
-  promoMovie: MoviePropType,
-  movies: PropTypes.arrayOf(MoviePropType),
-  onOpenMovieDetailsPage: PropTypes.func.isRequired,
-  onAddReviewButtonClick: PropTypes.func.isRequired,
+  //movies: PropTypes.arrayOf(MoviePropType),
   setDataError: PropTypes.func.isRequired,
   changeActivePage: PropTypes.func.isRequired,
-  changeActiveMovie: PropTypes.func.isRequired,
 };
 
 
 const mapStateToProps = (state) => ({
   dataError: getDataError(state),
   activePage: getActivePage(state),
-  movies: getMovies(state),
-  promoMovie: getPromoMovie(state),
+  //movies: getMovies(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  onOpenMovieDetailsPage(movie) {
-    dispatch(DataActionCtrator.setMaxMoviesCount(NUMBER_OF_SIMILAR_FILMS));
-    dispatch(ApplicationActionCreator.resetVisibleMoviesCount());
-    dispatch(ApplicationActionCreator.changeActiveMovie(movie));
-    dispatch(Operation.loadActiveMovieReviews(movie.id));
-    dispatch(ApplicationActionCreator.changeGenre(movie.genres[0]));
-    dispatch(ApplicationActionCreator.changeActivePage(PageType.MOVIE_DETAILS));
-  },
-  onAddReviewButtonClick() {
-    dispatch(ApplicationActionCreator.changeActivePage(PageType.ADD_REVIEW));
-    dispatch(DataActionCtrator.setDataError(null));
-  },
   setDataError(error) {
     dispatch(DataActionCtrator.setDataError(error));
   },
   changeActivePage(page) {
     dispatch(ApplicationActionCreator.changeActivePage(page));
-  },
-  changeActiveMovie(movie) {
-    dispatch(ApplicationActionCreator.changeActiveMovie(movie));
   },
 });
 
--- src/components/main-page/main-page.jsx
+++ src/components/main-page/main-page.jsx
@@ -1,37 +1,73 @@
-import React from "react";
+import PropTypes from "prop-types";
+import React, {PureComponent} from "react";
+import {connect} from "react-redux";
 
+import {ActionCreator as ApplicationActionCreator} from "../../reducers/application/application";
 import {Catalog} from "../catalog/catalog";
 import {FilterByGenre} from "../filter-by-genre/filter-by-genre";
 import {Footer} from "../footer/footer";
 import {MovieCardPromoWithPlayer} from "../movie-card-promo/movie-card-promo";
 import {MoviePropType} from "../../prop-types";
+import {PageType} from "../../const";
+import {getPromoMovie} from "../../reducers/data/selectors";
 
 
-export const MainPage = (props) => {
-  const {promoMovie} = props;
+class MainPageComponent extends PureComponent {
+  componentWillMount() {
+    this.props.onOpenMainPage(this.props.promoMovie);
+  }
 
-  return (
-    <React.Fragment>
-      <MovieCardPromoWithPlayer movie={promoMovie} />
+  componentDidUpdate() {
+    this.props.onOpenMainPage(this.props.promoMovie);
+  }
 
-      <div className="page-content">
-        <section className="catalog">
-          <h2 className="catalog__title visually-hidden">
-            Catalog
-          </h2>
+  render() {
+    const {promoMovie} = this.props;
 
-          <FilterByGenre />
+    return (
+      <React.Fragment>
+        <MovieCardPromoWithPlayer movie={promoMovie} />
 
-          <Catalog />
-        </section>
+        <div className="page-content">
+          <section className="catalog">
+            <h2 className="catalog__title visually-hidden">
+              Catalog
+            </h2>
 
-        <Footer />
-      </div>
-    </React.Fragment>
-  );
+            <FilterByGenre />
+
+            <Catalog />
+          </section>
+
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+
+MainPageComponent.propTypes = {
+  promoMovie: MoviePropType.isRequired,
+  onOpenMainPage: PropTypes.func.isRequired,
 };
 
 
-MainPage.propTypes = {
-  promoMovie: MoviePropType.isRequired,
+const mapStateToProps = (state) => ({
+  promoMovie: getPromoMovie(state),
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onOpenMainPage(promoMovie) {
+    dispatch(ApplicationActionCreator.changeActiveMovie(promoMovie));
+    dispatch(ApplicationActionCreator.changeActivePage(PageType.MAIN));
+  },
+});
+
+const MainPage = connect(mapStateToProps, mapDispatchToProps)(MainPageComponent);
+
+
+export {
+  MainPageComponent,
+  MainPage,
 };

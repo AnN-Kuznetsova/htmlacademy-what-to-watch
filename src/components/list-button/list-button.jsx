f112b0412f22ee090c80af493343b01da8fa8bc4
Анна Кузнецова <akimova_anna_25@mail.ru>
2020-08-05 23:10:28 +0300

    Добавит переход неавторизованных пользователей на страницу 'SignIn'.

--- src/components/list-button/list-button.jsx
+++ src/components/list-button/list-button.jsx
@@ -2,15 +2,34 @@ import PropTypes from "prop-types";
 import React from "react";
 import {connect} from "react-redux";
 
+import {AppRoute} from "../../const";
+import {AuthorizationStatus} from "../../reducers/user/user";
 import {MoviePropType} from "../../prop-types";
 import {getActiveMovie} from "../../reducers/application/selectors";
+import {getAuthorizationStatus} from "../../reducers/user/selectors";
+import {history} from "../../history";
 
 
 const ListButtonComponent = (props) => {
-  const {movie} = props;
+  const {
+    movie,
+    authorizationStatus,
+  } = props;
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    if (authorizationStatus === AuthorizationStatus.NO_AUTH) {
+      history.push(AppRoute.SIGN_IN);
+    }
+
+
+  };
 
   return (
-    <button className="btn btn--list movie-card__button" type="button">
+    <button className="btn btn--list movie-card__button" type="button"
+      onClick={handleClick}
+    >
       <svg viewBox="0 0 19 20" width="19" height="20">
         {movie.isFavorite
           && <use xlinkHref="#in-list" />
@@ -24,11 +43,13 @@ const ListButtonComponent = (props) => {
 
 ListButtonComponent.propTypes = {
   movie: MoviePropType.isRequired,
+  authorizationStatus: PropTypes.string.isRequired,
 };
 
 
 const mapStateToProps = (state) => ({
   movie: getActiveMovie(state),
+  authorizationStatus: getAuthorizationStatus(state),
 });
 
 /* const mapDispatchToProps = (dispatch) => ({
